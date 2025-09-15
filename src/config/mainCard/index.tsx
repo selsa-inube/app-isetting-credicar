16f2ca1acc4bbbb5d5172c2b63fc5ef183c9af26
angie pinilla <angiepinillanova@gmail.com>
2025-09-15 15:27:15 -0500

    fix: remove property in object of config

--- src/config/mainCard/index.tsx
+++ src/config/mainCard/index.tsx
@@ -3,22 +3,18 @@ import { ICardData } from "@ptypes/home/ICardData";
 const mainCards: ICardData[] = [
   {
     publicCode: "Líneas de Crédito",
-    icon: "",
     url: "/credit-lines",
   },
   {
     publicCode: "Destinos de dinero",
-    icon: "",
     url: "/money-destination",
   },
   {
     publicCode: "Nóminas de convenio",
-    icon: "",
     url: "/payroll-agreement",
   },
   {
     publicCode: "Politicas generales de credito",
-    icon: "",
     url: "/general-credit-policies",
   },
 ];
