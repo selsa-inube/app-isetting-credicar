e1ff07a22f0fa7df471a0b2dcd09844a85814273
cmarin001 <cmarin6051@gmail.com>
2025-09-26 14:35:00 -0500

    fix: update scroll behavior for dropdownmenu container

--- src/pages/creditLines/tabs/configurationLines/styles.ts
+++ src/pages/creditLines/tabs/configurationLines/styles.ts
@@ -8,6 +8,32 @@ const StyledDropdownMenuContainer = styled.div`
   width: 334px;
   background: ${({ theme }) =>
     `${theme?.palette?.neutral?.N20 || inube.palette.neutral.N20}`};
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) =>
+      `${theme?.palette?.neutral?.N80 || inube.palette.neutral.N80}`};
+    border-radius: 8px;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &:hover::-webkit-scrollbar,
+  &:hover::-webkit-scrollbar-thumb {
+    opacity: 1;
+    pointer-events: auto;
+  }
 `;
 
 export { StyledDropdownMenuContainer };
